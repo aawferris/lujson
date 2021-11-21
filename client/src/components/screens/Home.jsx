72380ca587160ce0e18c05c671ad4c8deb84c871
Andy Ferris <aawferris@aawferris.local>
2021-11-21 11:25:03 -0500

    add file structure

---
+++ client/src/components/screens/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from "../Card"
+import Header from "../shared/Header"
+import Footer from "../shared/Footer"
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <div id="main-box">
+      <Header />
+      <div id="main-card-box">
+        <Card />
+      </div>
+      <Footer />
+    </div>
+  )
+}
---
+++ client/src/components/shared/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <div>
+      <div id="footer-main">
+        <div id="shape-box">
+          <img
+            src="/assets/circle.png"
+            alt="black circle"
+            id="circle"
+          />
+          <img
+            src="/assets/triangle.png"
+            alt="black triangle"
+            id="triangle"
+          />
+          <img
+            src="/assets/square.png"
+            alt="black square"
+            id="square"
+          />
+          </div>
+        </div>
+    </div>
+  )
+}
